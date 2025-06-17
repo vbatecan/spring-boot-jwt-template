8aa0ceaa595a61961d8a95dc5bfac1d41e69fa5b
Vince Angelo Batecan <llTheBlankll@gmail.com>
2025-06-17 17:58:20 +0800

    Initial commit

---
+++ src/test/java/com/vbatecan/springboot_jwt/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbatecan.springboot_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
